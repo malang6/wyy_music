e8dcb7ea7accdc93586feb14a8ebd58f92ca8a34
unknown <1031966036@qq.com>
2021-01-05 18:41:46 +0800

    fetch:电台功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-const Djradio = () => import('@views/djradio')
-const Category = () => import('@views/category')
-const push = VueRouter.prototype.push
-VueRouter.prototype.push = function(location, onComplete, onAbort) {
-  if (onComplete && onAbort) {
-    return push.call(this, location, onComplete, onAbort)
-  }
-  return push.call(this, location, onComplete, () => {})
-}
-VueRouter.prototype.replace = function(location, onComplete, onAbort) {
-  if (onComplete && onAbort) {
-    return push.call(this, location, onComplete, onAbort)
-  }
-  return push.call(this, location, onComplete, () => {})
-}
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+const Djradio = () => import("@views/djradio");
+const Category = () => import("@views/category");
+const Artist = () => import("@views/artist");
+Vue.use(VueRouter);
 const router = new VueRouter({
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Djradio,
       children: [
         {
-          name: 'category',
-          path: '/category',
-          component: Category,
+          name: "category",
+          path: "/category",
+          component: Category
         },
-      ],
-    },
-  ],
-})
-export default router
+        {
+          name: "artist",
+          path: "/artist",
+          component: Artist
+        }
+      ]
+    }
+  ]
+});
+export default router;
